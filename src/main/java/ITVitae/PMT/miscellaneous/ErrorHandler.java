658cc96d5e1e34ada11bc9fac8a79f3bd1e53dbf
Sofia Klören <Sf.kloren@outlook.com>
2025-11-18 10:51:23 +0100

    errors now show http status

--- src/main/java/ITVitae/PMT/miscellaneous/ErrorHandler.java
+++ src/main/java/ITVitae/PMT/miscellaneous/ErrorHandler.java
@@ -1,15 +1,33 @@
 package ITVitae.PMT.miscellaneous;
 
+import org.springframework.data.crossstore.ChangeSetPersister;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
+
 public class ErrorHandler {
     public static <T> T throwError(Constants.Errors message){
-        return actuallyThrowError(message.toString());
+        return actuallyThrowError(message.toString(), message);
     }
 
     public static <T> T throwError(String prefix, Constants.Errors message){
-        return actuallyThrowError(prefix + message.toString());
+        return actuallyThrowError(prefix + message.toString(), message);
     }
 
-    private static <T> T actuallyThrowError(String message) {
-        throw new RuntimeException(message);
+    private static <T> T actuallyThrowError(String message, Constants.Errors type) {
+        throw new ResponseStatusException(status(type), message);
+        //throw new RuntimeException(message);
+    }
+
+    private static HttpStatus status(Constants.Errors type) {
+        switch (type) {
+            case NOT_FOUND:
+                return HttpStatus.NOT_FOUND;
+            case WRONG_PASSWORD:
+            case DENIED:
+            case ALREAD_EXISTS:
+                return HttpStatus.FORBIDDEN;
+            default:
+                return HttpStatus.INTERNAL_SERVER_ERROR;
+        }
     }
 }
